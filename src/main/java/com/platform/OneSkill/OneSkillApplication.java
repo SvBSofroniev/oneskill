47cf28f9b2e59b34d2dae5f2de57f6332bdcf564
Svetlin Sofroniev <svetlinbiserov@gmail.com>
2024-04-28 22:35:29 +0300

    Initial commit - Filled the app.

---
+++ src/main/java/com/platform/OneSkill/OneSkillApplication.java
@@ -0,0 +1,13 @@
+package com.platform.OneSkill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneSkillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneSkillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/OneSkill/OneSkillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.OneSkill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneSkillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
